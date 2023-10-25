d9e6fb39293802fd3edb66b695c6ff8fa6fd909e
maonaoda <maonaoda@gmail.com>
2023-10-25 23:19:40 +0900

    first commit
    change button name

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿
+using Reactive.Bindings;
+
+namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public ReactiveProperty<CustomWebViewSource> CustomWebViewSource { get; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Microsoft.Maui.Controls.NavigationPage.SetHasNavigationBar(this, false);
+
+            CustomWebViewSource = new ReactiveProperty<CustomWebViewSource>(new MauiApp2.CustomWebViewSource(null));
+
+            BindingContext = this;
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            CustomWebViewSource.Value = new CustomWebViewSource("https://github.com/");
+        }
+    }
+
+    public class CustomWebViewSource(string url) : WebViewSource
+    {
+        public string Url { get; set; } = url;
+
+        public override void Load(IWebViewDelegate renderer)
+        {
+            renderer.LoadUrl(Url);
+        }
+    }
+}
